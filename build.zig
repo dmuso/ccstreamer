94247d6ff8c63708d0fbc2a19148d6cde8f40e41
Dan Harper <dan@kingdomsolutions.com.au>
2025-08-25 05:29:09 +1000

    Release v0.1.12

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 // Version information
-const version = "0.1.11";
+const version = "0.1.12";
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
